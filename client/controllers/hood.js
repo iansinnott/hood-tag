8512caf74de5b3f58cb27e6a476921b4dcc64afe
Ian Sinnott <ian@iansinnott.com>
2015-01-12 21:17:01 -0800

    Add that SF map. Implemented in meteor.

---
+++ client/controllers/hood.js
@@ -0,0 +1,45 @@
+Template.hood.rendered = function() {
+  var paper = Raphael("sf-map-container", 680, 580);
+
+  var shapeAttr = {
+    "fill": "#7A1E48",
+    "stroke": "#111625",
+    "stroke-width": "0.6",
+    "stroke-linejoin": "round"
+  };
+
+
+  var nameAttr = {
+    "fill": "#fff",
+    "font-family": "Helvetica",
+    "font-size": 11,
+    "font-weight": "bold",
+    "text-anchor": "start"
+  };
+
+  Neighborhoods.find().forEach(function(hood) {
+    console.log('hey we made it');
+    console.log(hood);
+    drawNeighborhood(hood.path, hood.nameX, hood.nameY, hood.name, hood.slug);
+  });
+
+  function drawNeighborhood(_path, _nameX, _nameY, _name, _slug) {
+    var path = paper.path(_path).attr(shapeAttr),
+      name   = paper.text(_nameX, _nameY, _name).attr(nameAttr),
+      url    = _slug,
+      set    = paper.set();
+
+    set.push(path, name);
+    set.attr({ "href": url });
+
+    set.mouseover(function (event) {
+      this.attr({ "cursor": "pointer" });
+      path.animate({ fill: "#341931" }, 400);
+    });
+    set.mouseout(function (event) {
+      path.animate({ fill: "#7A1E48" }, 400);
+    });
+  }
+
+};
+
