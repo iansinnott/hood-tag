157d5cdf405e71ba32ac79dcbf0335c028ea7c3d
Ian Sinnott <ian@iansinnott.com>
2015-01-13 00:05:33 -0800

    Add a bit of much needed reactivity.

--- client/controllers/hood.js
+++ client/controllers/hood.js
@@ -1,3 +1,23 @@
+Template.hood.helpers({
+
+  taglines: function() {
+    return Taglines.find({ hoodId: Session.get('currentNeighborhood') });
+  },
+
+  currentNeighborhood: function() {
+    return Neighborhoods.findOne(Session.get('currentNeighborhood'));
+  }
+
+});
+
+Template.hood.events({
+  'click a': function(e, template) {
+    e.preventDefault();
+    console.log(template);
+    Session.set('currentNeighborhood', $(e.currentTarget).attr('href'));
+  }
+});
+
 Template.hood.rendered = function() {
   var paper = Raphael("sf-map-container", 680, 580);
 
@@ -18,19 +38,17 @@ Template.hood.rendered = function() {
   };
 
   Neighborhoods.find().forEach(function(hood) {
-    console.log('hey we made it');
-    console.log(hood);
-    drawNeighborhood(hood.path, hood.nameX, hood.nameY, hood.name, hood.slug);
+    drawNeighborhood(hood);
   });
 
-  function drawNeighborhood(_path, _nameX, _nameY, _name, _slug) {
-    var path = paper.path(_path).attr(shapeAttr),
-      name   = paper.text(_nameX, _nameY, _name).attr(nameAttr),
-      url    = _slug,
-      set    = paper.set();
+  function drawNeighborhood(hood) {
+    var path = paper.path(hood.path).attr(shapeAttr),
+        name = paper.text(hood.nameX, hood.nameY, hood.name).attr(nameAttr),
+        url  = hood.slug,
+        set  = paper.set();
 
     set.push(path, name);
-    set.attr({ "href": url });
+    set.attr({ "href": hood._id });
 
     set.mouseover(function (event) {
       this.attr({ "cursor": "pointer" });
